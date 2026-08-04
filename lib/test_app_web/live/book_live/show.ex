c5505026e79eba3c8a129bc06e8d0491a403b8d4
Dean Kinyua <kinyuadean@gmail.com>
2026-08-04 14:55:48 +0300

    access the resource via a loaded_resource assign

--- lib/test_app_web/live/book_live/show.ex
+++ lib/test_app_web/live/book_live/show.ex
@@ -1,43 +1,49 @@
 defmodule TestAppWeb.BookLive.Show do
   use TestAppWeb, :live_view
 
+  import TestApp.Authorization
+
   alias TestApp.Books
+
+  @impl true
+  def resource_module, do: TestApp.Books.Book
 
   @impl true
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
       <.header>
-        Book {@book.id}
+        Book {@loaded_resource.id}
         <:subtitle>This is a book record from your database.</:subtitle>
         <:actions>
           <.button navigate={~p"/books"}>
             <.icon name="hero-arrow-left" />
           </.button>
-          <.button variant="primary" navigate={~p"/books/#{@book}/edit?return_to=show"}>
-            <.icon name="hero-pencil-square" /> Edit book
-          </.button>
+
+          <%= if can(@current_scope.user) |> edit?(@loaded_resource) do %>
+            <.button variant="primary" navigate={~p"/books/#{@loaded_resource}/edit?return_to=show"}>
+              <.icon name="hero-pencil-square" /> Edit book
+            </.button>
+          <% end %>
         </:actions>
       </.header>
 
       <.list>
-        <:item title="Name">{@book.name}</:item>
-        <:item title="Pages">{@book.pages}</:item>
+        <:item title="Name">{@loaded_resource.name}</:item>
+        <:item title="Pages">{@loaded_resource.pages}</:item>
+        <:item title="Public">{@loaded_resource.public}</:item>
       </.list>
     </Layouts.app>
     """
   end
 
   @impl true
-  def mount(%{"id" => id}, _session, socket) do
+  def mount(%{"id" => _id}, _session, socket) do
     if connected?(socket) do
       Books.subscribe_books(socket.assigns.current_scope)
     end
 
-    {:ok,
-     socket
-     |> assign(:page_title, "Show Book")
-     |> assign(:book, Books.get_book!(socket.assigns.current_scope, id))}
+    {:ok, assign(socket, :page_title, "Show Book")}
   end
 
   @impl true
