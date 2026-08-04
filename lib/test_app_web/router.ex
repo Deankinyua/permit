0ecf4207c531bef4b4544a3a59306e13d3b3474a
Dean Kinyua <kinyuadean@gmail.com>
2026-08-04 13:44:44 +0300

    adding the AuthorizeHook will require each LiveView within the live session to implement some form of Authorization logic. Disable it for the settings LiveViews for now

--- lib/test_app_web/router.ex
+++ lib/test_app_web/router.ex
@@ -52,6 +52,14 @@ defmodule TestAppWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [
+        {TestAppWeb.UserAuth, :require_authenticated}
+      ] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    live_session :with_authorization,
+      on_mount: [
         {TestAppWeb.UserAuth, :require_authenticated},
         Permit.Phoenix.LiveView.AuthorizeHook
       ] do
@@ -59,9 +67,6 @@ defmodule TestAppWeb.Router do
       live "/books/new", BookLive.Form, :new
       live "/books/:id", BookLive.Show, :show
       live "/books/:id/edit", BookLive.Form, :edit
-
-      live "/users/settings", UserLive.Settings, :edit
-      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
     end
 
     post "/users/update-password", UserSessionController, :update_password
